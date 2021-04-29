b4e733a7017e6528f93c263f7f13e3ab209a1f89
wriabow <damnmeandyou@gmail.com>
2021-04-29 16:58:14 +0300

    глава параметры закончена

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
             int y = 222;
             Addition(ref x, y);
 
-            Console.WriteLine("Результат Sum: " + Sum(10, 15) + "\n");        // параметры передаются по значению]
+            Console.WriteLine("Результат Sum: " + Sum(10, 15) + "\n");        // параметры передаются по значению
             Console.WriteLine("Результат Addition: " + x + "\n");
             Console.ReadKey();
             Console.WriteLine("Сравнение передачи параметров. Вариант 1: передача по значению \n");
@@ -42,6 +42,14 @@ namespace ConsoleApp1
         {
             x++;
             Console.WriteLine($"IncrementRef: {x}");
+        }
+        // Пример модификатора "in"  который не даёт изменять входные параметры 
+        static void GetData(in int x, int y, out int area, out int perim)
+        {
+            // x = x + 10; нельзя изменить значение параметра x
+            y = y + 10;
+            area = x * y;
+            perim = (x + y) * 2;
         }
 
         // При передачи значений, функция получает не переменную а её копию
